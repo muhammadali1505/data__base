27cac7b1240b0437ffa00a54e9846973d2147c24
muhammadali1505 <muhammadaliinamov5554@gmail.com>
2022-08-08 08:58:25 +0300

    08.08.2022

---
+++ comfingwindow.py
@@ -0,0 +1,8 @@
+from tkinter import *
+# head
+root = Tk()
+root.title("Calculator")
+root.geometry("300x400")
+root.resizable(False,False)
+# body
+root.mainloop()
---
+++ main.py
@@ -0,0 +1,5 @@
+from tkinter import *
+# head
+root = Tk()
+# body
+root.mainloop()
